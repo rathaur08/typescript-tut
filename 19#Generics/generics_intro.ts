a7c32ce3e200d7575e04d75d6cf5b5f1353d4890
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-11-20 15:55:57 +0530

    Update Code - TS Generics: Multiple Type Variables

--- 19#Generics/generics_intro.ts
+++ 19#Generics/generics_intro.ts
@@ -11,10 +11,21 @@ console.log(output1); // Output: Hello, Generics!
 console.log(output2); // Output: 42
 
 // 2nd Example: Generic Interface
-interface GenericIdentityFn<T> {
-  (arg: T): T;
+function add<T, U>(a: T, b: U): void {
+  console.log(typeof a);
+  console.log(typeof b);
 }
 
-let myIdentity: GenericIdentityFn<number> = identity;
+const result1 = add(5, 8); // Both arguments are numbers
+const result2 = add("Hello, ", "Generics!");
 
-console.log(myIdentity(100)); // Output: 100
+// 3rd Example:
+function addValue<T, U, V>(a: T, b: U, c: V): void {
+  console.log(typeof a);
+  console.log(typeof b);
+  console.log(typeof c);
+}
+
+const result11 = addValue(5, 8, true); // Both arguments are numbers
+const result22 = addValue("Hello, ", "Generics!", 80);
+const result33 = addValue("Hello, ", "Generics!", false);
