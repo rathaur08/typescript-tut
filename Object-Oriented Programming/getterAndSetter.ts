9d5227ff3d6f82a5541fb82c687ee5ff608e1982
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-11-28 12:18:47 +0530

    Code - Building Bank Account & Temperature Converter Program in TypeScript -#28

--- Object-Oriented Programming/getterAndSetter.ts
+++ Object-Oriented Programming/getterAndSetter.ts
@@ -2,34 +2,85 @@
 
 // Example
 
-class Persons {
-  private _age: number | undefined;
+// class Persons {
+//   private _age: number | undefined;
 
-  constructor(public name: string, protected hobbies: string[]) {}
+//   constructor(public name: string, protected hobbies: string[]) {}
 
-  public set age(age: number) {
-    if (age > 150 || age < 0) {
-      throw new Error("age is not valid");
-    }
-    this._age = age;
+//   public set age(age: number) {
+//     if (age > 150 || age < 0) {
+//       throw new Error("age is not valid");
+//     }
+//     this._age = age;
+//   }
+
+//   public get age() {
+//     if (this._age === undefined) {
+//       throw new Error("age is not defind");
+//     }
+//     return this._age;
+//   }
+
+//   greet(): string {
+//     return `Hello, my name is ${this.name} and I am ${
+//       this._age
+//     } years old. If you like, my hobbies are ${this.hobbies.join(", ")}.`;
+//   }
+// }
+
+// const person1 = new Persons("Alice", 30, ["painting", "traveling"]);
+// person1.age = 537;
+// console.log(person1.greet());
+
+// practice Quetions
+
+// Bank Account Balance -------------
+class BankAccount {
+  private _balance: number = 0;
+
+  public get balance(): number {
+    return this._balance;
   }
 
-  public get age() {
-    if (this._age === undefined) {
-      throw new Error("age is not defind");
+  public set balance(newBalance: number) {
+    if (newBalance < 0) {
+      throw new Error("Invald Balance");
     }
-    return this._age;
-  }
-
-  greet(): string {
-    return `Hello, my name is ${this.name} and I am ${
-      this._age
-    } years old. If you like, my hobbies are ${this.hobbies.join(", ")}.`;
+    this._balance = newBalance;
   }
 }
 
-const person1 = new Persons("Alice", 30, ["painting", "traveling"]);
+const account = new BankAccount();
+account.balance = 10;
+console.log(account.balance);
 
-person1.age = 537;
+// Temperature Converter --------------
 
-console.log(person1.greet());
+class Temperature {
+  private _celsius: number = 0;
+
+  public get celsius(): number {
+    return this._celsius;
+  }
+
+  public set celsius(newCelcius: number) {
+    this._celsius = newCelcius;
+  }
+
+  public get fahrenheit(): number {
+    return (this._celsius * 9) / 5 + 32;
+  }
+
+  public set fahrenheit(newFahrenheit: number) {
+    this._celsius = ((newFahrenheit - 32) * 5) / 9;
+  }
+}
+
+const temp: any = new Temperature();
+temp.celsius = 25;
+console.log(temp.fahrenheit);
+
+
+temp.fahrenheit = 77;
+console.log(temp.celsius);
+
